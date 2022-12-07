7431ee929dba8c6308539b1db4c39248a9383a5f
Carmen Chow <carmendevelops@gmail.com>
2022-12-07 10:38:46 -0500

    server

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const express = require('express');
 const app = express();
 
 // Set up middleware with use();
-// Anything starting with the first argument '/products', will be handled by 'productRoutes' and forwarded to the product page:
-app.use('/products', productRoutes);
+app.use((req, res, next) => {
+  res.status(200).json({
+    message: 'It works!'
+  });
+});
 
 module.exports = app;
--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 const http = require('http');
 const app = require('./app');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 000;
 const server = http.createServer(app);
 server.listen(port);
+
+// In terminal: node server.js then go to localhost:3000 to see message 'It works!' from app.js
